a38630448df2ab27d42fd1d1496003824fc32436
cmanage1 <55030581+cmanage1@users.noreply.github.com>
2019-09-30 21:42:52 -0400

    Add files via upload

---
+++ GetMiddleChar.py
@@ -0,0 +1,11 @@
+#GetMiddleCharacter
+
+#You are going to be given a word. Your job is to return the middle character
+#of the word. If the word's length is odd, return the middle character.
+#If the word's length is even, return the middle 2 characters.
+
+def get_middle(s):
+    if len(s) % 2 == 0:
+        return s[len(s)/2-1] + s[len(s)/2]
+    else:
+        return (s[len(s)/2])
